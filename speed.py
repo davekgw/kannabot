e425321dc131b601a1f7e70673a2c0ba36f2a020
DaveKgw1 <45153558+davekgw@users.noreply.github.com>
2022-08-14 22:35:18 +0700

    Add files via upload

--- speed.py
+++ speed.py
@@ -1970,7 +1970,7 @@ def shell():
                 ((results.upload / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]),
                 quiet)
-        printer("\n\n• ʙʏ ʟᴇᴛᴛᴀ - sᴀᴍᴀ •")
+        printer("\n\n• ʙʏ ꜰᴀɴɢᴢ ɢᴀɴᴢ •")
     else:
         printer('Skipping upload test', quiet)
 
